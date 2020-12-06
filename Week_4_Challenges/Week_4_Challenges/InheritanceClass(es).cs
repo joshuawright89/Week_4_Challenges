7d91a31dceaac52d54b3bd9ea85b65f39f1db5da
Joshua Wright <joshuacarrwright@gmail.com>
2020-12-05 23:25:34 -0500

    Completed Push

---
+++ Week_4_Challenges/Week_4_Challenges/InheritanceClass(es).cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_Challenges
+{
+    //Chal_1 -- Create an abstract class **CHECK**
+    //Chal_2 -- Give three properties & one virtual or **abstract method**
+    abstract class Book
+    {
+        public string Title { get; set; }
+        public string Author_First { get; set; }
+        public string Author_Last { get; set; }
+        public Book() { }
+        public Book(string title, string author_First, string author_Last)
+        {
+            Title = title;
+            Author_First = author_First;
+            Author_Last = author_Last;
+        }
+    }
+
+    //Chal_3 -- Declare at least two classes that will inherit from abstract class Book
+    class FictionBook : Book
+    {
+        public bool IsBoringAsHell { get; set; }
+        public FictionBook() { }
+        public FictionBook(bool isBoringAsHell, string title, string author_First, string author_Last)
+            : base(title, author_First, author_Last)
+        {
+            IsBoringAsHell = isBoringAsHell;
+        }
+
+        
+    }
+
+    class NonFictionBook : Book
+    {
+        public bool IsBiographical { get; set; }
+    }
+}
---
+++ Week_4_Challenges/Week_4_Challenges/Wk_4_Challenges_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week_4_Challenges
+{
+    [TestClass]
+    public class Wk_4_Challenges_Tests
+    {
+        [TestMethod]
+        public void BookExamples()
+        {
+
+            //"...new up an instance of each class that inherits from the parent class you set up"
+            //instance #1 -- FICTION
+            var fictionBook = new FictionBook(true, "Empire of the Sun", "J.G.", "Ballard");
+            fictionBook.Title = "";
+            fictionBook.Author_First = "";
+            fictionBook.Author_Last = "";
+            fictionBook.IsBoringAsHell = true;
+
+            //instance #2 -- NONFICTION
+            var nonFictionBook = new NonFictionBook();
+            nonFictionBook.Title = "All Quiet on the Western Front";
+            nonFictionBook.Author_First = "Erich";
+            nonFictionBook.Author_Last = "Remarque";
+            nonFictionBook.IsBiographical = true;
+
+
+        }
+    }
+}
